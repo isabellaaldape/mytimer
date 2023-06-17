639b4f7610c6763986e9596037c81b6c03ebc0fa
Isabella Aldape <isabella@aldape.dev>
2023-06-17 14:43:54 -0700

    added random surprise

---
+++ index.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+
+    // disable initial buttons
+    document.getElementById("btn-pause").disabled = true;
+    document.getElementById("btn-stop").disabled = true;
+
+    // define global variables
+    var timer = null; 
+    var h = 0;
+    var m = 0;
+    var s = 0;
+
+};
+
+function rickRolled() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+
+}
